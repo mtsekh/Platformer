35ca8d2f6116e3ea43f8badd526b0e2d750a6832
mtsekh <m.tsekhmistro@gmail.com>
2018-10-18 17:19:07 +0300

    sceleton on OOP

---
+++ main.py
@@ -0,0 +1,49 @@
+import pygame as pg
+from settings import *
+
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((800, 600))
+        pg.display.set_caption('platformer game')
+        self.clock = pg.time.Clock()
+
+        self.running = True
+    def new(self):
+        self.all_sprites = pg.sprite.Group()
+        self.run()
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.playing = False
+                self.running = False
+
+    def updates(self):
+        self.all_sprites.update()
+
+
+    def draw(self):
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.updates()
+            self.draw()
+
+    def main(self):
+        while self.running:
+            self.new()
+
+
+    def __del__(self):
+        pg.quit()
+
+g = Game()
+g.main()
